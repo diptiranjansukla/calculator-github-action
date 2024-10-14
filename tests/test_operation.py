49ebde5511f6fe364d7df8d6afdc13aa2e9512ca
diptiranjansukla <diptiranjanshukla1998@gmail.com>
2024-10-14 17:42:45 +0530

    multiplication added

--- tests/test_operation.py
+++ tests/test_operation.py
@@ -1,4 +1,4 @@
-from src.math import add,sub
+from src.math import add,sub,mul
 
 def test_add():
     assert add(2,3)==5
@@ -9,4 +9,11 @@ def test_sub():
     assert sub(3,3)==0
     assert sub(8,3)==5
     assert sub(9,3)==6
+
+
+def test_mul():
+    assert mul(3,3)==9
+    assert mul(8,3)==24
+    assert mul(9,3)==27
+
 
